f72966a461bbb5177b31b157aebbf466de553d3d
Irbah28 <71394775+Irbah28@users.noreply.github.com>
2020-12-12 16:32:58 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<HTML DOCTYPE=html>
+<html>
+
+<head>
+</head>
+<?php
+    include("navbar.php");
+?>
+
+
+
+<body>
+    <div id='wall'>
+        <ul id="new-feeds">
+            <li>Hello, World!</li>
+        </ul>
+    </div>
+</body>
+
+
+
+</html>
+
+
+
